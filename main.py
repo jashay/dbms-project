d2f08f472fee17f077800ddf9d0465e356c315c2
Jashay Lilani <jashay.lilani@gmail.com>
2021-12-13 02:46:51 +0530

    admin dashboard almost complete

--- main.py
+++ main.py
@@ -2,11 +2,26 @@ import pymysql
 from app import app
 from config import mysql
 from flask import jsonify
-from flask import flash, request
+from flask import flash, request, render_template
 import random
 
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/admin')
+def adminpage():
+	return render_template('admin.html')
+
+@app.route('/user')
+def userpage():
+	return render_template('user.html')
+
+# ADMIN
+
 @app.route('/customers')
-def customer():
+def get_all_customers():
 	try:
 		conn = mysql.connect()
 		cursor = conn.cursor(pymysql.cursors.DictCursor)
@@ -22,6 +37,109 @@ def customer():
 		cursor.close() 
 		conn.close()
 
+@app.route('/airlines')
+def getairlines():
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor(pymysql.cursors.DictCursor)
+		cursor.execute("SELECT * from smjl_airlines")
+		rows = cursor.fetchall()
+		respone = jsonify(rows)
+		respone.status_code = 200
+		return respone
+	except Exception as e:
+		print(e)
+		return None
+	finally:
+		cursor.close() 
+		conn.close()
+
+@app.route('/passengers')
+def get_all_passengers():
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor(pymysql.cursors.DictCursor)
+		cursor.execute("SELECT * from smjl_passenger")
+		rows = cursor.fetchall()
+		respone = jsonify(rows)
+		respone.status_code = 200
+		return respone
+	except Exception as e:
+		print(e)
+		return None
+	finally:
+		cursor.close() 
+		conn.close()
+
+@app.route('/flights')
+def get_all_flights():
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor(pymysql.cursors.DictCursor)
+		cursor.execute("SELECT * from smjl_flight")
+		rows = cursor.fetchall()
+		respone = jsonify(rows)
+		respone.status_code = 200
+		return respone
+	except Exception as e:
+		print(e)
+		return None
+	finally:
+		cursor.close() 
+		conn.close()
+
+@app.route('/toptwocustomers') 
+def get_top_two_customers():
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor(pymysql.cursors.DictCursor)
+		cursor.execute("SELECT p_id, count(smjl_customer_p_id) as numberOfBookings from smjl_customer, smjl_booking where p_id = smjl_customer_p_id group by p_id order by numberOfBookings desc limit 2")
+		rows = cursor.fetchall()
+		respone = jsonify(rows)
+		respone.status_code = 200
+		return respone
+	except Exception as e:
+		print(e)
+		return None
+	finally:
+		cursor.close() 
+		conn.close()
+
+@app.route('/toptwoairlines') 
+def get_top_two_airlines():
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor(pymysql.cursors.DictCursor)
+		cursor.execute("SELECT al_name, count(f_id) from smjl_airlines, smjl_flight where al_id=smjl_airlines_al_id group by al_name order by count(f_id) desc limit 2;")
+		rows = cursor.fetchall()
+		respone = jsonify(rows)
+		respone.status_code = 200
+		return respone
+	except Exception as e:
+		print(e)
+		return None
+	finally:
+		cursor.close() 
+		conn.close()
+
+@app.route('/toptwomemberships') 
+def get_top_two_memberships():
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor(pymysql.cursors.DictCursor)
+		cursor.execute("SELECT m_id, m_name, count(p_id) from smjl_club_memberships group by m_id order by count(p_id) desc limit 2;")
+		rows = cursor.fetchall()
+		respone = jsonify(rows)
+		respone.status_code = 200
+		return respone
+	except Exception as e:
+		print(e)
+		return None
+	finally:
+		cursor.close() 
+		conn.close()
+		
+
 @app.errorhandler(404)
 def not_found(error=None):
     message = {
@@ -31,6 +149,8 @@ def not_found(error=None):
     respone = jsonify(message)
     respone.status_code = 404
     return respone
+
+# USER
 
 @app.route('/addcustomer', methods=['POST'])
 def add_cust():
@@ -44,7 +164,6 @@ def add_cust():
 		_emergency_lname=_json['emergency_lname']	
 		_emergency_phone=_json['emergency_phone']	
 		_password = _json['password']
-		print(_nationality, _phone, _passengers_alongside)
 		_fname = _json['fname']
 		_lname = _json['lname']
 		_dob = _json['dob']
@@ -55,7 +174,7 @@ def add_cust():
 		if request.method == 'POST':	
 			p_id = random.randint(1, 10000)		
 
-			sqlQuery1 = "INSERT INTO smjl_passenger VALUES(%s, %s, %s, STR_TO_DATE(%s, '%M %d %Y'), %s, %s, %s, STR_TO_DATE(%s, '%M %d %Y'))"
+			sqlQuery1 = "INSERT INTO smjl_passenger VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"
 			bindData1 = (p_id, _fname, _lname, _dob, _nationality, _gender, _passport_number, _passport_expiry)
 			print(p_id)
 			sqlQuery = "INSERT INTO smjl_customer VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)"
@@ -79,6 +198,24 @@ def add_cust():
 		conn.close()
 
 
+@app.route('/bookings/<id>') 
+def get_bookings(id):
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor(pymysql.cursors.DictCursor)
+		query = "SELECT * from smjl_booking where smjl_customer_p_id = %s"
+		bData = (id)
+		cursor.execute(query, bData)
+		rows = cursor.fetchall()
+		respone = jsonify(rows)
+		respone.status_code = 200
+		return respone
+	except Exception as e:
+		print(e)
+		return None
+	finally:
+		cursor.close() 
+		conn.close()
 
 if __name__ == "__main__":
     app.run()
